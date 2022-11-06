8c494b1976afe8981f0b63a1f7c6b01ba0be2a0f
Denis <svd.disic5@mail.ru>
2022-11-06 19:51:50 +0300

    test1 - feature -1

--- src/main/java/CodeTest.java
+++ src/main/java/CodeTest.java
@@ -10,8 +10,7 @@ public class CodeTest {
         int[]array = {1,2,4,4,5,67,12,12};
         Arrays.sort(array);
         int n  = array.length;
-        int n2  = array.length;
-        int n3  = array.length;
+
         int[] a = Arrays.copyOfRange(array,0,(n+1) / 2);
         int[] b = Arrays.copyOfRange(array,(n+1) / 2, n);
 
@@ -20,8 +19,7 @@ public class CodeTest {
 
 
         System.out.println();
-        System.out.println();
-        System.out.println();
+
 
 
 
