7471bda2d352157b5026c42b5282e5b40bfb8615
Denis <svd.disic5@mail.ru>
2022-11-06 20:54:01 +0300

    squash

--- src/main/java/CodeTest.java
+++ src/main/java/CodeTest.java
@@ -10,19 +10,13 @@ public class CodeTest {
         int[]array = {1,2,4,4,5,67,12,12};
         Arrays.sort(array);
         int n  = array.length;
-        int n2  = array.length;
-        int n3  = array.length;
+
         int[] a = Arrays.copyOfRange(array,0,(n+1) / 2);
         int[] b = Arrays.copyOfRange(array,(n+1) / 2, n);
 
         System.out.println(Arrays.toString(a));
         System.out.println(Arrays.toString(b));
 
-
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
         System.out.println();
         System.out.println();
 
